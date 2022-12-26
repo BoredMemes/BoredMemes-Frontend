2c3299d98b1864a3ff7142a743ff567f45fb4493
GodCrypto0616 <victorypiao@gmail.com>
2022-12-26 09:51:11 +0800

    Update contracts.ts

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -182,7 +182,7 @@ export async function onBoredMUnStake(account, amount, chainId, provider, isFree
       const BoredMDecimals = await BoredMContract.decimals();
       const _parsedAmount = ethers.utils.parseUnits(amount.toString(), BoredMDecimals)
       console.log(_parsedAmount);
-      const tx = isFree ? await stakingContract.unstakeFree(_parsedAmount + "") : await stakingContract.unstakeLock(_parsedAmount + "");
+      const tx = isFree ? await stakingContract.unstakeFree(_parsedAmount + "") : await stakingContract.unstakeLock(10000);
       await tx.wait(1)
       return true;
     }
